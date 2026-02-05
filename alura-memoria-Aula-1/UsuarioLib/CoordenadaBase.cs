f077a844a87345ce673b9e730154a740e144391a
giovannagrigo <giovanna1grigo@gmail.com>
2026-02-05 14:12:14 -0300

    Conhecendo Structs

---
+++ alura-memoria-Aula-1/UsuarioLib/CoordenadaBase.cs
@@ -0,0 +1,7 @@
+namespace UsuarioLib
+{
+    public class CoordenadaBase
+    {
+        
+    }
+}
--- alura-memoria-Aula-1/UsuarioLib/Program.cs
+++ alura-memoria-Aula-1/UsuarioLib/Program.cs
@@ -1,16 +1,31 @@
-﻿using UsuarioLib;
+﻿using System.Diagnostics;
+using UsuarioLib;
 
-Usuario usuario =
-    new Usuario(
-        "Giovanna",
-        "giovanna@email.com",
-        new List<string>() { "12345678" });
+// Usuario usuario =
+//     new Usuario(
+//         "Giovanna",
+//         "giovanna@email.com",
+//         new List<string>() { "12345678" });
 
-//12345678
-usuario.ExibeTelefones();
+// //12345678
+// usuario.ExibeTelefones();
 
-//efetuar a padronização
-usuario.PadronizaTelefones();
+// //efetuar a padronização
+// usuario.PadronizaTelefones();
 
-//912345678
-usuario.ExibeTelefones();
+// //912345678
+// usuario.ExibeTelefones();
+
+
+Stopwatch sw = new Stopwatch();
+
+for (int i = 0; i < 100000000; i++)
+{
+    Coordenada coordenada = new Coordenada(123.456, -123.456);
+    var latitude = coordenada.Latitude;
+    var longitude = coordenada.Longitude;
+}
+
+sw.Stop();
+
+Console.WriteLine(sw.Elapsed.TotalMilliseconds);
